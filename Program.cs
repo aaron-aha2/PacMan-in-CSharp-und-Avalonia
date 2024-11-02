8e1adbbac0bba36c368a24d3556d730680615fc8
Verena Schelling <verena.schelling@student.reutlingen-university.de>
2024-11-02 22:25:59 +0100

    Spielfeld ausgelagert und vergrößert

--- Program.cs
+++ Program.cs
@@ -2,18 +2,7 @@
 
 class Program
 {
-    static char[,] spielfeld = { //'#' als Wand und '.' als einsammelbaren Punkt 
-        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
-        { '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#' },
-        { '#', '.', '#', '#', '#', '#', '.', '#', '.', '#', '#', '#', '#', '#', '.', '#', '#', '.', '#' },
-        { '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#' },
-        { '#', '#', '#', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '#', '.', '#' },
-        { '#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#' },
-        { '#', '.', '#', '#', '#', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '#', '#' },
-        { '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#' },
-        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
-    };
-
+    static Spielfeld spielfeld = new Spielfeld();
     static int pacmanX = 1; //Startposition von Pacman (X-Koordinate)
     static int pacmanY = 1; //Startposition von Pacman (Y-Koordinate)
 
@@ -24,21 +13,9 @@ class Program
         while (spielLaeuft) //Hauptschleife des Spiels
         {
             Console.Clear(); //Löscht die Konsole, um das Spielfeld zu aktualisieren bzw. neu zu zeichen
-            ZeichneSpielfeld(); 
+            spielfeld.ZeichneSpielfeld(); 
             ConsoleKey eingabe = Console.ReadKey(true).Key; //Liest Eingabe ein ohne Konsolenausgabe 
             BewegePacman(eingabe); 
-        }
-    }
-
-    static void ZeichneSpielfeld() 
-    {
-        for (int y = 0; y < spielfeld.GetLength(0); y++) //Schleife Zeilen
-        {
-            for (int x = 0; x < spielfeld.GetLength(1); x++) //Schleife Spalte
-            {
-                Console.Write(spielfeld[y, x]); //Ausgabe 2D-Array "Spielfeld" 
-            }
-            Console.WriteLine(); //Neue Zeile nach jeder Zeile im Spielfeld
         }
     }
 
@@ -63,12 +40,12 @@ class Program
                 break;
         }
 
-        if (spielfeld[neueY, neueX] != '#') //Prüfen, ob es eine Wand ist
+        if (spielfeld.Feld[neueY, neueX] != '#') //Prüfen, ob es eine Wand ist
         {
-            spielfeld[pacmanY, pacmanX] = ' '; //Alte Position cleanen
+            spielfeld.Feld[pacmanY, pacmanX] = ' '; //Alte Position cleanen
             pacmanX = neueX; //Koordinaten werden neu gesetzt 
             pacmanY = neueY;
-            spielfeld[pacmanY, pacmanX] = 'P'; //Pacman auf die neue Position setzen
+            spielfeld.Feld[pacmanY, pacmanX] = 'P'; //Pacman auf die neue Position setzen
         }
     }
 }
--- obj/Debug/net8.0/INFO - Pacman.AssemblyInfo.cs
+++ obj/Debug/net8.0/INFO - Pacman.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFO - Pacman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5116318b170b53c601a6dddc769cb9bd334f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFO - Pacman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFO - Pacman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
