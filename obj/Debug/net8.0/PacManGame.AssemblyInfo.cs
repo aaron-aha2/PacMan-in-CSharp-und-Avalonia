47b8294aab5af0f494f5dfc80124667f7475cfcf
Verena Schelling <vereschelling@googlemail.com>
2024-11-15 15:05:28 +0100

    Code-Update

--- obj/Debug/net8.0/PacManGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/PacManGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PacManGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b1692fe3bb55f0a799d32a426ea0d402f8eba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ecee18d920fccaedfa4bbd86f74b7ca0a955e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PacManGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PacManGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
